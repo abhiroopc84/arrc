c54160b33bfe169acb639341536c7c7d64e91166
abhiroopc84 <abhiroopc84@icloud.com>
2025-01-03 13:10:36 +0530

    Update logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,26 @@
 "use client";
 
-import Background from "@/components/background";
-import Projects from "@/components/projects";
-import Resume from "@/components/resume";
-import Skills from "@/components/skills";
-import { Button } from "@/components/ui/button";
-import home from "@/config/home";
-import { Mail } from "lucide-react";
+import AsteriskCard from "@/components/asterisk-ui";
+import MainCard from "@/components/main";
+import MapCard from "@/components/map-card";
+import Socials from "@/components/socials";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 h-full flex-col gap-8 pt-8">
-      <div className="flex flex-col">
-        {home.description.map((line) => (
-          <span key={line}>{line}</span>
-        ))}
-      </div>
-      <div className="flex-1 flex flex-col gap-8">
-        <Projects />
-        <Skills />
-        <Resume />
-      </div>
-      <Background />
-      <div>
-        <Button size={"icon"} variant={"default"} asChild>
-          <a href={home.email} className="flex flex-row items-center gap-2">
-            <Mail className="h-4 w-4" />
-          </a>
-        </Button>
+    <div className="flex flex-col xl:flex-row gap-8 min-h-dvh xl:h-dvh w-full p-8">
+      <MainCard />
+      <div className="flex flex-col gap-8 h-1/2 flex-1 xl:h-full xl:w-1/2 justify-between">
+        <div className="flex flex-col md:flex-row gap-8 overflow-hidden h-fit w-full">
+          <AsteriskCard />
+          <MapCard />
+        </div>
+        {/* <div
+            className="flex flex-col gap-4 w-full rounded-3xl p-10"
+            style={{ backgroundImage: bgimageurl }}
+          >
+            <span className="text-lg font-semibold">Check out my blog!</span>
+          </div> */}
+        <Socials />
       </div>
     </div>
   );
