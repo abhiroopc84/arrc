54a81a288ae0f2f028b2b93c08735d44dfc692d1
abhiroopc84 <abhiroopc84@icloud.com>
2025-01-03 13:08:18 +0530

    Add mail to socials config

--- src/config/socials.tsx
+++ src/config/socials.tsx
@@ -1,6 +1,12 @@
+import { MailIcon } from "lucide-react";
 import { TbBrandGithub, TbBrandLinkedin, TbBrandX } from "react-icons/tb";
 
 const socials = [
+  {
+    name: "mail",
+    link: "mailto:abhiroopc84@gmail.com",
+    icon: <MailIcon className="w-5 h-5" />,
+  },
   {
     name: "github",
     link: "https://github.com/abhiroopc84",
